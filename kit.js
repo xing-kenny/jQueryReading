99a2c3c2a1b696896fbcacac5b1789395232de5c
xing-kenny <xing_kenny@yahoo.com.cn>
2012-12-19 11:39:17 +0800

    ...

---
+++ kit.js
@@ -0,0 +1,3 @@
+kit:
+1.
+foo.call(this, arg1,arg2,arg3) == foo.apply(this, arguments)==this.foo(arg1, arg2, arg3)
---
+++ seqOfQunit.js
@@ -0,0 +1,45 @@
+This is qunit qunit.js:12
+This is QUnit.module ___ name = core qunit.js:325
+ selector state is valid ....... qunit.js:371
+This is Test.prototype.queue qunit.js:273
+This is before of synchronize(function() {	test.init();}); in Test.prototype.queue qunit.js:277
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is after of synchronize(function() {	test.init();}); in Test.prototype.queue qunit.js:283
+This is before of synchronize( run, true ) in Test.prototype.queue qunit.js:312
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is after of synchronize( run, true ) in Test.prototype.queue qunit.js:314
+This is QUnit.asyncTest qunit.js:332
+This is QUnit.module ___ name = callbacks qunit.js:325
+This is QUnit.load qunit.js:951
+this is extend( QUnit, init qunit.js:699
+This is QUnit.start qunit.js:386
+This is process qunit.js:1302
+This is process.config.queue.shift()() qunit.js:1311
+This is start of test.init() in Test.prototype.queue qunit.js:279
+This is Test.prototype.init qunit.js:47
+This is end of test.init() in Test.prototype.queue qunit.js:281
+This is process.config.queue.shift()() qunit.js:1311
+This is start of run() in Test.prototype.queue qunit.js:286
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is synchronize(callback, last) start qunit.js:1292
+This is synchronize(callback, last) end qunit.js:1298
+This is end of run() in Test.prototype.queue qunit.js:300
+This is process.config.queue.shift()() qunit.js:1311
+This is Test.prototype.setup qunit.js:70
+This is process.config.queue.shift()() qunit.js:1311
+This is Test.prototype.run qunit.js:121
+This is QUnit.expect qunit.js:377
+This is process.config.queue.shift()() qunit.js:1311
+This is Test.prototype.teardown qunit.js:153
+This is process.config.queue.shift()() qunit.js:1311
+This is Test.prototype.finish qunit.js:168
+This is process.done() qunit.js:1320
+This is function done()  start qunit.js:1113
+This is function done()  end 
